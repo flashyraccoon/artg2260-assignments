0193cd5cf463a7166a6b652140f8951a680a36ac
flashyraccoon <flashyraccoon@users.noreply.github.com>
2018-01-26 14:12:37 -0500

    sdrgtdv

--- sketch_tile.js
+++ sketch_tile.js
@@ -1,5 +1,5 @@
 function setup() {
-	createCanvas(1000, 1000);
+	createCanvas(100, 100);
 
 	for (var y = 0; y < 100; y += 10){
 		for (var x = 0; x < 100; x += 10){
@@ -7,14 +7,9 @@ function setup() {
       noStroke();
       fill(255, 246, 0);
 			ellipse(x, y, 10, 10);
+			stroke(0);
+			line(x+(x*1), x+(x*1), x+10, x+10*x);
 		}
 	}
-
-  function mouseMoved() {
-      g = g + 5;
-      if (g > 255) {
-        g = 0;
-  }
-}
 
 }
