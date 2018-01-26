5b39615f51999dd9019b37ed0e7dc83a6482b94e
flashyraccoon <flashyraccoon@users.noreply.github.com>
2018-01-26 17:36:35 -0500

    good progress
    fixed the weird gap in the bottom navbar, and fixed the issue of the content flowing over the header when scrolling.

--- sketch_tile.js
+++ sketch_tile.js
@@ -1,14 +1,14 @@
 function setup() {
 	createCanvas(100, 100);
 
-	for (var y = 0; y < 100; y += 10){
-		for (var x = 0; x < 100; x += 10){
+	for (var y = 0; y < 10; y += 1){
+		for (var x = 0; x < 10; x += 1){
       ellipseMode(CORNER);
       noStroke();
       fill(255, 246, 0);
-			ellipse(x, y, 10, 10);
+			ellipse(10*x, 10*y, 10, 10);
 			stroke(0);
-			line(x+(x*1), x+(x*1), x+10, x+10*x);
+			line(10*x+x, 10*y+y, (x+1)*10-x, (y+1)*10-y);
 		}
 	}
 
