2351858e0f78fd3ad451d44ea0ec3940761b9ab4
flashyraccoon <flashyraccoon@users.noreply.github.com>
2018-01-25 21:44:28 -0500

    slfj

--- sketch_tile.js
+++ sketch_tile.js
@@ -1,8 +1,20 @@
 function setup() {
-  createCanvas (800, 500);
-  stroke(255);
+	createCanvas(1000, 1000);
+
+	for (var y = 0; y < 100; y += 10){
+		for (var x = 0; x < 100; x += 10){
+      ellipseMode(CORNER);
+      noStroke();
+      fill(255, 246, 0);
+			ellipse(x, y, 10, 10);
+		}
+	}
+
+  function mouseMoved() {
+      g = g + 5;
+      if (g > 255) {
+        g = 0;
+  }
 }
 
-function draw() {
-  
 }
