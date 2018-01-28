e77d8a6f40388c2a4dc5cae0a9fbed799497d52c
flashyraccoon <flashyraccoon@users.noreply.github.com>
2018-01-28 17:28:17 -0500

    drawpad: working prototype
    - change color
    - change strokeWeight
    - switch between solid and dotted line
    - clear entire canvas
    - can't draw over panels
    - right panel shows brush size and color

---
+++ drawpad.js
@@ -0,0 +1,144 @@
+var weight = 1;
+var clrBtnColorR = 0;
+var clrBtnColorG = 0;
+var clrBtnColorB = 0;
+var drawDottedLine = true;
+
+
+function setup() {
+	var myCanvas = createCanvas( 1000, 500);
+	myCanvas.parent("canvas");
+	stroke(0, 0, 0); // we moved the default stroke color to setup()
+	strokeWeight(weight);
+	background(255, 255, 255);
+
+
+}
+
+function draw(){
+
+	noFill();
+	strokeWeight(1);
+	stroke(0);
+	rect(0, 0, 25, 75); //panel for solid line button
+
+	stroke(0);
+	strokeWeight(10);
+	line(12.5, 12.5, 12.5, 60); // line for solid line button
+
+	strokeWeight(1);
+	rect(0, 75, 25, 75); //panel for dotted line button
+	fill(0);
+	ellipseMode(CORNER);
+	ellipse(7.5, 85, 10, 10);
+	ellipse(7.5, 100, 10, 10);
+	ellipse(7.5, 115, 10, 10);
+	ellipse(7.5, 130, 10, 10);
+
+	noFill();
+	strokeWeight(1);
+	stroke(0);
+	rect(0, 475, 25, 24); //panel for clearing the canvas
+	strokeWeight(5);
+	strokeCap(ROUND);
+	stroke(255, 0, 0);
+	line(5, 480, 20, 494);
+	line(20, 480, 5, 494); // red X for clearing button
+
+
+
+	//draws the circle showing the brush size (right-hand panel)
+	ellipseMode(CENTER);
+	noStroke();
+	fill(clrBtnColorR, clrBtnColorG, clrBtnColorB);
+	ellipse(987.5, 12.5, weight, weight);
+
+	// draws the rect showing the brush color (right-hand panel)
+	fill (clrBtnColorR, clrBtnColorG, clrBtnColorB);
+	rect(975, 25, 25, 500);
+
+
+	if (mouseIsPressed == true) {
+		// checking if the mouse isn't hovering over the right- or left-hand panels
+		if(((mouseX > 50) || (mouseY > 175)) && (mouseX < 950)) {
+			if (drawDottedLine == false) {
+				strokeWeight(weight);
+			 	stroke(clrBtnColorR, clrBtnColorG, clrBtnColorB);
+			 	line(mouseX, mouseY, pmouseX, pmouseY);
+			} else if (drawDottedLine == true){
+			 	ellipse(mouseX, mouseY, weight, weight);
+		 	}
+		}
+	}
+}
+
+function mouseReleased() {
+	// checking if the mouse is hovering over the solid-line-button (left-hand panel)
+	if ((mouseX > 0) && (mouseX < 26) && (mouseY > 0) && (mouseY < 76)) {
+		if (drawDottedLine == true) {
+			drawDottedLine = false; }
+	// checking if the mouse is hovering over the dotted-line-button (left-hand panel)
+	} else if ((mouseX > 0 ) && (mouseX < 26) && (mouseY > 75) && (mouseY < 151)){
+			if(drawDottedLine == false) {
+				drawDottedLine = true; }
+	// checking if the mouse is hovering over the clear-all panel (left-hand panel)
+	} else if ((mouseX > 0 ) && (mouseX < 26) && (mouseY > 475) && (mouseY < 500)){
+			stroke(255);
+			fill(255);
+			rect(0, 0, 1000, 500);
+		}
+	}
+
+function keyPressed() {
+	if (key == "c" || key == "C") { //cyan
+	    stroke(0, 255, 255);
+	    clrBtnColorR = 0;
+	    clrBtnColorG = 250;
+	    clrBtnColorB = 250;
+		} else if (key == "m" || key == "M") { //magenta
+	    stroke(255, 0, 255);
+	    clrBtnColorR = 255;
+	    clrBtnColorG = 0;
+	    clrBtnColorB = 255;
+	  } else if (key == "y" || key == "Y") { //yellow
+	    stroke(255, 255, 0);
+	    clrBtnColorR = 255;
+	    clrBtnColorG = 255;
+	    clrBtnColorB = 0;
+	  } else if (key == "k" || key == "K") { //black
+	    stroke(0, 0, 0);
+	    clrBtnColorR = 0;
+	    clrBtnColorG = 0;
+	    clrBtnColorB = 0;
+	  } else if (key == "r" || key == "R") { //red
+	    stroke(255, 0, 0);
+	    clrBtnColorR = 255;
+	    clrBtnColorG = 0;
+	    clrBtnColorB = 0;
+	  } else if (key == "g" || key == "G") { //green
+	    stroke(0, 255, 0);
+	    clrBtnColorR = 0;
+	    clrBtnColorG = 250;
+	    clrBtnColorB = 0;
+	  } else if (key == "b" || key == "B") { //blue
+	    stroke(0, 0, 255);
+	    clrBtnColorR = 0;
+	    clrBtnColorG = 0;
+	    clrBtnColorB = 255;
+	  } else if (key == "w" || key == "W") {
+	    stroke(255,255,255); // transparent/white-out
+	    clrBtnColorR = 255;
+	    clrBtnColorG = 255;
+	    clrBtnColorB = 255;
+		} else if (keyCode == RIGHT_ARROW) {
+			weight++;
+			strokeWeight(weight);
+		} else if (keyCode == LEFT_ARROW) {
+			ellipseMode(CENTER);
+			fill(255);
+			stroke(255);
+			ellipse(987.5, 12.5, weight, weight); //paints a white circle over the old, bigger brushsize
+			weight/=2;
+			strokeWeight(weight);
+		}
+}
