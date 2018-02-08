101879641a7891e5a8dd794c960a4841d6d6ca63
flashyraccoon <flashyraccoon@users.noreply.github.com>
2018-02-07 20:15:06 -0500

    irrlicht slideshow

--- javascript.js
+++ javascript.js
@@ -1,3 +1,37 @@
+$(document).ready(function(){
+    $('.carousel').carousel({
+      interval: 2000
+    })
+  });   
+
+$(document).ready(function(){
+    // Activate Carousel
+    $("#myCarousel").carousel();
+
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#myCarousel").carousel(2);
+    });
+    $(".item4").click(function(){
+        $("#myCarousel").carousel(3);
+    });
+
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#myCarousel").carousel("next");
+    });
+});
+
+// ---------------------------------------------------
 
   function myFunction() {
     var x = document.getElementById("myNavbar");
