473d6fb574619bcefc4e4eda62d3b6a42e1045f8
flashyraccoon <flashyraccoon@users.noreply.github.com>
2018-02-21 14:55:41 -0500

    game assignment
    working on it

--- unicornpoacher.js
+++ unicornpoacher.js
@@ -1,16 +1,22 @@
 let gameState = 0;
 let score = 0;
 let poacher;
+let unicorns = [];
 let width = 500;
 let height = 500;
-let a;
+let a = 0;
 let bullets = [];
-let time = framecount % framerate;
+let timePassed;
+let intervals = [90, 210, 300];
+let lives = 3;
 
 
 function setup(){
   framerate = 30;
   createCanvas(500,500);
+  let img_heart_full = loadImage("images\assets\heart-full.png");
+  let img_heart_empty = loadImage("images\assets\heart-empty.png");
+
   poacher = new Poacher();
 }
 
@@ -20,17 +26,32 @@ function draw(){
     startScreen();
   } else if (gameState == 1){
     update();
+    for (i = 0; i < lives; i++){
+      img(img_heart_full, 40+i, 20, 20, 20);
+    }
+    poacher.display();
+    poacher.move();
+    for (b of bullets) {
+      b.move();
+      b.display();
+      print("bullet comes!");
+    }
+    for (u of unicorns) {
+      u.move();
+      u.display();
+    }
+    timePassed = frameCount % random(intervals);
+    if (timePassed == 0) {
+      print("Time passed is " + timePassed);
+      let unicorn = new Unicorn();
+      unicorns.push (unicorn);
+    }
   } else if (gameState == 2){
     gameOver();
   }
-  poacher.display();
-  poacher.move();
-  for (b of bullets) {
-    b.move();
-    b.display();
-    print("bullet comes!");
-  }
+
 }
+
 
 function mouseClicked(){
    if (gameState == 0){
@@ -44,12 +65,14 @@ function mouseClicked(){
 
 function startScreen() {
   background(255);
+  fill(0);
   text("click to begin", 10, 30);
 }
 
 function update() {
   background(255);
   score++;
+  fill(0);
   text("playing", 10, 30);
   text("score: " + score, 400, 30);
 }
@@ -69,14 +92,16 @@ class Poacher {
     this.x = width/2;
     this.y = height/2;
     this.diameter = 20;
-    this.regX = this.x;
-    this.regY = this.y;
     this.a = 0;
+    this.color = 255;
+    this.outline = 0;
   }
 
   display(){
  // put a conditional for a -> only in game state 1
     this.a = Math.atan2(mouseY - this.y, mouseX - this.x);
+    fill(this.color);
+    stroke(this.outline);
     translate(this.x, this.y);
     rotate(this.a);
     beginShape();
@@ -102,22 +127,24 @@ class Poacher {
     }
 
     shoot(){
-      let bullet = new Bullet(this.a);
+      let bullet = new Bullet();
       bullets.push (bullet);
     }
 
   }
 
 class Bullet {
-  constructor(_a){
+  constructor(){
     this.x = 0;
     this.y = 0;
     this.diameter = 5;
-    this.a = _a;
+    //this.a = poacher.a;
+    this.color = 0;
+
   }
 
   display(){
-    fill(0);
+    fill(this.color);
     ellipse(this.x, this.y, this.diameter, this.diameter);
   }
 
@@ -127,22 +154,23 @@ class Bullet {
 }
 
 class Unicorn {
+
   constructor(){
-    this.x = random(0||width);
-    this.y = random(height);
+    this.x = 0;
+    this.y = 50;
     this.diameter = 20;
+    this.color = 155;
+    this.alpha = 50;
+
   }
 
   display(){
-    fill(100,100,100);
-    ellipse(this.x, this.y, this.diameter, this.diameter);
+    fill(this.color, this.alpha);
+    ellipse(20, 20, this.diameter, this.diameter);
   }
 
   move(){
-    if(this.x == 0){
-      x+= 4;
-    } else if(this.x == wifth){
-      x-= 4;
-    }
+    this.x += 4;
+    this.y += 4;
   }
 }
