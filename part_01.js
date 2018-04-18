c0dfbec03d37ab3083450d6641dd8b7b73a8676a
flashyraccoon <flashyraccoon@users.noreply.github.com>
2018-04-17 21:27:59 -0400

    doneeeeeee

---
+++ part_01.js
@@ -0,0 +1,22 @@
+// function initMap() {
+//         var dorchesterHeights = {lat: 42.332901, lng: -71.045754};
+//         var start = {lat: 42.332644, lng: -71.072117};
+//         var map = new google.maps.Map(document.getElementById('map'), {
+//           zoom: 15,
+//           streetViewControl: false,
+//           mapTypeControlOptions: false,
+//           center: start
+//         });
+//       }
+//
+//         var marker_01 = new google.maps.Marker({
+//           position: start,
+//           map: map
+//         });
+//
+//         var marker_02 = new google.maps.Marker({
+//           position: dorchesterHeights,
+//           map: map
+//         });
+//
+//       }
